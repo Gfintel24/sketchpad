7d17752dd1290bff105a5f8a882701fae1cf2475
Garrett Fintel <gfintel24@hotmail.com>
2017-08-16 21:28:30 -0600

    Added buttons and pen functionality.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Going to try and create the sketchpad/etch-n-sketch on a browser utilizing html, css, and javascript/jquery.
+A project intended to replicate the sketchpad/etch-n-sketch on a browser utilizing html, css, and javascript/jquery.
+
+Pre-project Thoughts: Seems intimidating. We'll see how it turns out.
+
+Post-project Thoughts: Overall, it was a lot of fun designing this sketchpad. I feel I spent too much time going down rabbit holes trying to find any way to make what I wanted to do possible. A few of these ideas turned out to be too big for the scope of this project. Such as having a border radius on the corners, no matter the size of the grid. Also, correctly adding 10% of black to a square, even if it already had a color in it. I didn't want to reset the square to white and go to black. I wanted to actually decrease the lighting of the square, while keeping the existing hue and saturation. I will probably come back and try and implement these ideas later on, but for now, I'm calling it finished.
--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,38 @@
 $(document).ready(function(){
+  //Initial global variable declaration.
   var numberOfRows = 8;
-  var btnMargin = ($('body').width()) / 2;
+  var penStyle = 0;
+  var blackPen = 0;
+  var rainbowPen = 1;
+  var gradPen = 2;
 
+  //Create initial grid based off numberOfRows.
   createGridByTable();
 
-  $('.newPad').css("margin", "0 " + btnMargin + "px");
-
-  $('.newPad').click(function(){
-    // var currentRows = numberOfRows;
+  //When clicked, prompts user for size of new pad, empties the table, and then calls the createGride function.
+  $('#newPad').click(function(){
     numberOfRows = prompt ("How many squares, on each side, would you like?");
-    // if (currentRows > numberOfRows){
-      $(table).empty();
-      createGridByTable();
-    // }
-    // elseif (numberOfRows > currentRows){
-    //   increaseGrid();
-    // }
+    $(table).empty();
+    createGrid();
   });
 
-  function createGridByTable(){
+  //Event listener for the rainbow button to change the pen style.
+  $('#rainbow').click(function(){
+    penStyle = rainbowPen;
+  })
+
+  //Need to spend more time learning how to implement this function correctly.
+  // $('#gradient').click(function(){
+  //   penStyle = gradPen;
+  // })
+
+  //Event listener for the black pen button to change the pen style.
+  $('#black').click(function(){
+    penStyle = blackPen;
+  })
+
+  //The work horse of the script. All calculated variables are called inside the function.
+  function createGrid(){
     var sqWidth = ($('#container').width()) / numberOfRows;
     var sqHeight = ($('#container').height()) / numberOfRows;
     var tableRow = ("<tr class='row'></tr>");
@@ -26,9 +40,9 @@ $(document).ready(function(){
     var rowCount = 0;
     var dataCount = 0;
 
+    //Appends table rows and data elements to the HTML table creating the grid.
     while (rowCount < numberOfRows){
       $('#table').append(tableRow);
-      $('')
       while (dataCount < numberOfRows){
         $('.row').last().append(tableData);
         dataCount++
@@ -36,28 +50,40 @@ $(document).ready(function(){
       dataCount = 0;
       rowCount++
     }
+    
+    //Sets row and data element dimensions based on requested rows.
     $('.row').height(sqHeight);
     $('.data').width(sqWidth);
     $('.data').height(sqHeight);
 
+    //Switch statement for the pen functionality.
     $("td").on("mouseenter", function(){
-        $(this).css("background-color", "black");
+      switch(penStyle){
+        case 0:
+          $(this).css("background-color", "black");
+          break;
+
+        case 1:
+          var r = Math.floor((Math.random() * 255) + 1);
+          var g = Math.floor((Math.random() * 255) + 1);
+          var b = Math.floor((Math.random() * 255) + 1);
+
+          $(this).css("background-color", "rgb(" + r + "," + g + "," + b + ")");
+          break;
+
+        //Need to spend more time learning how to implement this function correctly.
+
+        // case 2:
+        // var lightness = $(this).getAttribute(brightness);
+        //   console.log(lightness);
+        // // $(this).css("background-color", "hsl(0, 0%, )")
+        //
+        //   break;
+
+        default:
+          $(this).css("background-color", "black");
+      }
+
     });
   }
-
-  // function(){
-  //   var rowsToMake = numberOfRows - currentRows;
-  //   var rowCount = 0;
-  //   var dataCount = 0;
-  //
-  //   while (rowCount < numberOfRows){
-  //     $('#table').append(tableRow);
-  //     while (dataCount < numberOfRows){
-  //       $('.row').last().append(tableData);
-  //       dataCount++
-  //     }
-  //     dataCount = 0;
-  //     rowCount++
-  //   }
-  // }
 });
